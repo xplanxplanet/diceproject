53763a6d5ca01ba692728fa04b9823b9f540106a
Ezeuko Chimeremeze <ezeukochimeremeze@gmail.com>
2023-06-13 22:22:00 +0100

    case sensitivity

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function rollTheDice() {
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    document.querySelector(".img1").setAttribute("src", "images/" + "dice" + randomNumber1 + ".png");
-    document.querySelector(".img2").setAttribute("src", "images/" + "dice" + randomNumber2 + ".png");
+    document.querySelector(".img1").setAttribute("src", "images/" + "Dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/" + "Dice" + randomNumber2 + ".png");
 
     if (randomNumber1 == randomNumber2) {
         document.querySelector("h1").innerHTML= "Draw";
